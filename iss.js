05d5f2eaf7d63f638d16907a5c6a52f8e2cc0c10
“Victoria <victoriahall.dev@gmail.com>
2021-12-25 00:27:23 +0000

    Implement api call to fetch ip address

---
+++ iss.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const fetchMyIP = function (callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null)
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
